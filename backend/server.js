bccb02026ccf15fd93e7c8afa54b51553779c66a
Zhan S <zhan.shi.f@gmail.com>
2023-05-21 11:31:13 +1000

    add backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+// Under TopSocial. 
+// Create folder backend, 
+// create file .gitignore which contains "node_modules/" ---> to let git ignore too many files in node_modules
+
+// cd backend
+// add db.json sent from lecturer. 
+// Create server.js
+// npm init
+    // package name: (backend) 
+    // version: (1.0.0) 
+    // description: top social backend
+    // entry point: (index.js) server.js
+// npm i express axios cors json-server  --- json server as database 
+// npm i nodemon concurrently -D  --- concurrently is used to combine commands 
+
+// Go to github and create a repo for the project. 
+// go to TopSocial 
+// git init 
+// git remote add origin https://github.com/zhanshiaus/TopSocial.git
+// git remote -v
